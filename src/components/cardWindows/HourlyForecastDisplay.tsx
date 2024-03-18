2e95e4c8fa13847701ad895d6f35136fc726371b
AdrianBinkley <adrianbinkley@gmail.com>
2024-03-18 11:53:54 -0400

    add hourly forecast scrolling

--- src/components/cardWindows/HourlyForecastDisplay.tsx
+++ src/components/cardWindows/HourlyForecastDisplay.tsx
@@ -1,9 +1,11 @@
+import PerfectScrollbar from 'react-perfect-scrollbar';
 import { useSelector } from 'react-redux';
 
 import { GeneralUnitType, WindspeedUnitType } from '../../app/stationData';
 import { HourlyForecast, convertWindSpeed, imageForWeatherCode, precipitationIconByWeatherCode } from '../../functions';
 import { RootState } from '../../pages';
 
+import 'react-perfect-scrollbar/dist/css/styles.css';
 import './HourlyForecastDisplay.css';
 
 export function HourlyForecastDisplay(props: { data: HourlyForecast[] }) {
@@ -12,38 +14,38 @@ export function HourlyForecastDisplay(props: { data: HourlyForecast[] }) {
 
 	const tempUnit = generalUnitType === 'english' ? '°F' : '°C';
 
-	// TODO: add nav arrows
-
 	return (
 		<div className='hourly-forecast-wrapper unselectable floating-window'>
-			{props.data.map((forecastDatum) => (
-				<div className='hourly-forecast-datum-wrapper' key={`hourly-forecast-datum-${forecastDatum.time}`}>
-					<p className='hourly-forecast-datum-label'>{forecastDatum.time}</p>
-					<p className='hourly-forecast-datum-temp'>{`${Math.round(forecastDatum.temp)} ${tempUnit}`}</p>
-					<div className='hourly-forecast-datum-weather-icon-wrapper'>
-						{imageForWeatherCode(precipitationIconByWeatherCode(forecastDatum.weatherCode, forecastDatum.isDay))}
-					</div>
-					<p className='hourly-forecast-datum-precipitation-chance'>
-						{forecastDatum.precipitationChance === 0 ? '' : `${forecastDatum.precipitationChance}%`}
-					</p>
-					<div className='hourly-forecast-datum-wind-data-wrapper'>
-						<div className='hourly-forecast-datum-wind-direction'>
-							<div
-								className='hourly-forecast-wind-arrow-wrapper'
-								style={{
-									transform: `rotate(${forecastDatum.wind.direction}deg)`
-								}}
-							>
-								<i className='bi bi-arrow-up hourly-forecast-wind-arrow' />
-							</div>
+			<PerfectScrollbar>
+				{props.data.map((forecastDatum) => (
+					<div className='hourly-forecast-datum-wrapper' key={`hourly-forecast-datum-${forecastDatum.time}`}>
+						<p className='hourly-forecast-datum-label'>{forecastDatum.time}</p>
+						<p className='hourly-forecast-datum-temp'>{`${Math.round(forecastDatum.temp)} ${tempUnit}`}</p>
+						<div className='hourly-forecast-datum-weather-icon-wrapper'>
+							{imageForWeatherCode(precipitationIconByWeatherCode(forecastDatum.weatherCode, forecastDatum.isDay))}
 						</div>
-						<p className='hourly-forecast-datum-wind-speed-number'>{`${Math.round(
-							convertWindSpeed(forecastDatum.wind.speed, windspeedUnitType)
-						)}`}</p>
+						<p className='hourly-forecast-datum-precipitation-chance'>
+							{forecastDatum.precipitationChance === 0 ? '' : `${forecastDatum.precipitationChance}%`}
+						</p>
+						<div className='hourly-forecast-datum-wind-data-wrapper'>
+							<div className='hourly-forecast-datum-wind-direction'>
+								<div
+									className='hourly-forecast-wind-arrow-wrapper'
+									style={{
+										transform: `rotate(${forecastDatum.wind.direction}deg)`
+									}}
+								>
+									<i className='bi bi-arrow-up hourly-forecast-wind-arrow' />
+								</div>
+							</div>
+							<p className='hourly-forecast-datum-wind-speed-number'>{`${Math.round(
+								convertWindSpeed(forecastDatum.wind.speed, windspeedUnitType)
+							)}`}</p>
+						</div>
+						<p className='hourly-forecast-datum-wind-speed-unit'>{windspeedUnitType}</p>
 					</div>
-					<p className='hourly-forecast-datum-wind-speed-unit'>{windspeedUnitType}</p>
-				</div>
-			))}
+				))}
+			</PerfectScrollbar>
 		</div>
 	);
 }
